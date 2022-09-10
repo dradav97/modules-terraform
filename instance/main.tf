125790a3c6febd4aa99cc9d691141f7ef59e4153
David Vargas <dradav.97@gmail.com>
2022-09-10 04:39:05 -0500

    fix: change egress value in dynamic rules security group

--- instance/main.tf
+++ instance/main.tf
@@ -32,10 +32,10 @@ resource "aws_security_group" "ssh_conection" {
   dynamic "egress" {
     for_each = var.egress_rules
     content {
-      from_port        = ingress.value.from_port
-      to_port          = ingress.value.to_port
-      protocol         = ingress.value.protocol
-      cidr_blocks      = ingress.value.cidr_blocks
+      from_port        = egress.value.from_port
+      to_port          = egress.value.to_port
+      protocol         = egress.value.protocol
+      cidr_blocks      = egress.value.cidr_blocks
     }
   }
 
